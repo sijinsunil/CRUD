0d9c974a2854e44c29a007727f5f91443d6d9c14
SIJIN S <66131714+sijinsunil@users.noreply.github.com>
2020-09-11 09:35:56 +0530

    URL update

--- crudexample/student/urls.py
+++ crudexample/student/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    
     path('', views.home,name='home'),
     path('update/<int:id>/', views.update, name='updatedata'),
     path('delete/<int:pk>/', views.delete, name='deletedata'),
